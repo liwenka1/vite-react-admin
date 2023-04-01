b1f1c5e585f996b72529b47d8dae3c1da20e46a5
liwenkai <2020583117@qq.com>
2023-04-01 23:51:59 +0800

    feat: 引入antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
 import { useState } from 'react'
 import CompOne from './components/CompOne'
 import CompTwo from './components/CompTwo'
+import { Button, Space } from 'antd'
+import { StepBackwardOutlined } from '@ant-design/icons'
 
 function App() {
   const [count, setCount] = useState(0)
+  const handClick = () => {
+    setCount((count) => {
+      return count + 1
+    })
+  }
 
   return (
     <div className="App">
       top
+      {count}
+      <button onClick={handClick}>点我佳佳</button>
       <CompOne></CompOne>
       <CompTwo></CompTwo>
+      <Space wrap>
+        <Button type="primary">Primary Button</Button>
+        <Button>Default Button</Button>
+        <Button type="dashed">Dashed Button</Button>
+        <Button type="text">Text Button</Button>
+        <Button type="link">Link Button</Button>
+        <StepBackwardOutlined />
+      </Space>
     </div>
   )
 }
