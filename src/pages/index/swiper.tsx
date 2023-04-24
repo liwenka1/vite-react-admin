3840bf32dbf293d6fa4d7b599ac41d110c5763b7
liwenkai <2020583117@qq.com>
2023-04-25 00:09:11 +0800

    fix: swiper白屏待解决

--- src/pages/index/swiper.tsx
+++ src/pages/index/swiper.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React from 'react'
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react'
 import type { Banner } from '@/models/banner'
@@ -8,7 +8,7 @@ import 'swiper/css'
 import 'swiper/css/pagination'
 
 // import required modules
-import { Pagination } from 'swiper'
+import { Autoplay, Navigation, Pagination, A11y } from 'swiper'
 
 interface Props {
   bannnerList: Banner[]
@@ -18,16 +18,22 @@ const swiper: React.FC<Props> = (props) => {
   return (
     <>
       <Swiper
+        slidesPerView={5}
         spaceBetween={30}
+        loop={true}
+        centeredSlides={true}
         pagination={{
-          clickable: true
+          dynamicBullets: true
         }}
-        modules={[Pagination]}
-        className="mySwiper"
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false
+        }}
+        modules={[Autoplay, Navigation, Pagination, A11y]}
       >
-        {props.bannnerList.map((item, index) => (
-          <SwiperSlide key={index}>
-            <img src={item.pic} />
+        {props.bannnerList.map((item) => (
+          <SwiperSlide key={item.bannerId}>
+            <img style={{ width: '100%' }} src={item.pic} />
           </SwiperSlide>
         ))}
       </Swiper>
